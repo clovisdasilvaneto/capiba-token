971cd43df30f17a176906f83d33e93cb1c364621
Clovis Neto <clovisdasilvaneto@gmail.com>
2024-04-03 14:57:58 +0900

    feat: add token to github

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {  
+    const MyToken = await hre.ethers.getContractFactory("MyToken");
+    const myToken = await MyToken.deploy(100000000, 50);
+    await myToken.deployed();
+  
+    console.log("Ocean token deployed: ", myToken.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
